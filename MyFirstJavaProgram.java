80cda9bb3c223cf4b4c6a761da75c640a7a23c80
Matthew Werner <matwer@outlook.com>
2021-06-04 14:05:54 -0700

    java fun...

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstJavaProgram {
+    public static void main (String [] args){
+        System.out.println("My name is Matthew Werner");
+        System.out.println("I am 52 years old");
+        System.out.println("My hometown is Kaukauna, WI");
+    }
+}
